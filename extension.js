eb5d47db28c91800b9d45f5f5e81ba1656721f2f
Philipp <philipp.thiele.94@gmail.com>
2022-03-31 19:26:30 +0200

    add boop scripts

--- extension.js
+++ extension.js
@@ -3,67 +3,70 @@
 const vscode = require("vscode");
 const fs = require("fs");
 const path = require("path");
-let customScripts = [];
 let quickPickScriptList = [];
+
+function addScriptsInPath(path) {
+    let scripts = fs.readdirSync(path);
+    for (let i = 0; i < scripts.length; i++) {
+      let item = scripts[i];
+      quickPickScriptList.push({
+        label: item,
+        description: item,
+		scriptPath: (path + "/" + item).replace(/\\/g, "/"),
+      });
+    }
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  addScriptsInPath(__dirname + "/Boop/Boop/Boop/Scripts");
   const settings = vscode.workspace.getConfiguration("voop");
   if (settings.customScriptsFolderLocation) {
-    //get files in customScriptsFolderLocation
-    customScripts = fs.readdirSync(settings.customScriptsFolderLocation);
-    for (let i = 0; i < customScripts.length; i++) {
-      let item = customScripts[i];
-      quickPickScriptList.push({
-        label: item,
-        description: item,
-      });
-    }
+	addScriptsInPath(settings.customScriptsFolderLocation);
   }
   //settings.customScriptsFolderLocation => default === null
 
   let disposable = vscode.commands.registerCommand("voop", function () {
-	vscode.window.showQuickPick(quickPickScriptList).then(selectedScript => {
-		// the user canceled the selection
-		if (!selectedScript) {
-		  return;
-		}
+    vscode.window.showQuickPick(quickPickScriptList).then((selectedScript) => {
+      // the user canceled the selection
+      if (!selectedScript) {
+        return;
+      }
 
-		const activeEditor = vscode.window.activeTextEditor;
-		if (!activeEditor) {
-		  return;
-		}
-		const document = activeEditor.document;
-	
-		const wholeDocumentText = document.getText();
-		const selectedText = document.getText(activeEditor.selection);
+      const activeEditor = vscode.window.activeTextEditor;
+      if (!activeEditor) {
+        return;
+      }
+      const document = activeEditor.document;
 
-		let textToEdit = !selectedText || selectedText.length === 0 ? wholeDocumentText : selectedText;
+      const wholeDocumentText = document.getText();
+      const selectedText = document.getText(activeEditor.selection);
 
-		const scriptPath = path.join(settings.customScriptsFolderLocation, selectedScript.label);
-		const script = fs.readFileSync(scriptPath, "utf8");
-		eval(script);
-		let inputObj = { 
-			text: textToEdit, 
-			selection: selectedText, 
-			fullText: wholeDocumentText,
-			postInfo: vscode.window.showInformationMessage,
-			postError: vscode.window.showErrorMessage
-		};
-		main(inputObj);
+      let textToEdit = !selectedText || selectedText.length === 0 ? wholeDocumentText : selectedText;
 
-		activeEditor.edit((editBuilder) => {
-		  if (!selectedText || selectedText.length === 0) {
-			//no selection, replace whole document
-			editBuilder.replace(new vscode.Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end), inputObj.text);
-		  } else {
-			//text is selected, replace selection
-			editBuilder.replace(activeEditor.selection, inputObj.text);
-		  }
-		});
-	  });
+      const script = fs.readFileSync(selectedScript.scriptPath, "utf8");
+      eval(script);
+      let inputObj = {
+        text: textToEdit,
+        selection: selectedText,
+        fullText: wholeDocumentText,
+        postInfo: vscode.window.showInformationMessage,
+        postError: vscode.window.showErrorMessage,
+      };
+      main(inputObj);
+
+      activeEditor.edit((editBuilder) => {
+        if (!selectedText || selectedText.length === 0) {
+          //no selection, replace whole document
+          editBuilder.replace(new vscode.Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end), inputObj.text);
+        } else {
+          //text is selected, replace selection
+          editBuilder.replace(activeEditor.selection, inputObj.text);
+        }
+      });
+    });
   });
 
   context.subscriptions.push(disposable);
