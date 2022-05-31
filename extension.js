b224516e6283334e6037d2f8b4196b5a80fa8919
Philipp <philipp.thiele.94@gmail.com>
2022-05-31 18:19:34 +0200

    add possibility to set a github repository as custom script source

--- extension.js
+++ extension.js
@@ -3,7 +3,10 @@
 const vscode = require("vscode");
 const fs = require("fs");
 const path = require("path");
-require('module-alias/register');
+const https = require("https");
+const yauzl = require("yauzl");
+const { settings } = require("cluster");
+require("module-alias/register");
 let quickPickScriptList = [];
 
 function addScriptsInPath(path) {
@@ -15,23 +18,145 @@ function addScriptsInPath(path) {
       const scriptContent = fs.readFileSync(scriptPath, "utf8");
       try {
         const declaration = JSON.parse(scriptContent.substring(scriptContent.indexOf("{"), scriptContent.indexOf("}") + 1).replace(/,\s+}$/, "}"));
-        if(!quickPickScriptList.find(s => s.label === declaration.name)){
+        if (!quickPickScriptList.find((s) => s.label === declaration.name)) {
           quickPickScriptList.push({
-            label: `${declaration.name}${declaration.userInput ? ' 👤' : ''}`,
+            label: `${declaration.name}${declaration.userInput ? " 👤" : ""}`,
             description: declaration.description,
             detail: declaration.tags,
             scriptPath: scriptPath,
             userInput: declaration.userInput,
-            userInputPlaceHolder: declaration.userInputPlaceHolder
+            userInputPlaceHolder: declaration.userInputPlaceHolder,
           });
         } else {
-          console.debug(`Voop: Script with name '${declaration.name}' exists twice, not adding second instance.`)
+          console.debug(`Voop: Script with name '${declaration.name}' exists twice, not adding second instance.`);
         }
       } catch (e) {
         console.error(`Voop: Couldn't load script ${item}`, e);
       }
     }
   }
+}
+
+function requestOptionsFromUrl(url, githubPersonalAccessToken) {
+  const requestUrl = new URL(url);
+  const requestOptions = {
+    hostname: requestUrl.hostname,
+    port: requestUrl.port,
+    path: requestUrl.pathname,
+    headers: {
+      "User-Agent": "NodeJs",
+      accept: "application/vnd.github.v3+json",
+    },
+  };
+  if(githubPersonalAccessToken && githubPersonalAccessToken.trim().length > 0) {
+    requestOptions.headers.Authorization = `token ${githubPersonalAccessToken}`
+  }
+  if(requestUrl.search){
+    requestOptions.path += requestUrl.search;
+  }
+  return requestOptions;
+}
+
+function downloadRepositoryAsZip(repoUrl, githubPersonalAccessToken) {
+  const githubUrlWithoutHttps = repoUrl.substring(repoUrl.indexOf("//") + 2);
+  const githubDomain = githubUrlWithoutHttps.substring(0, githubUrlWithoutHttps.indexOf("/"));
+  const ownerAndRepo = githubUrlWithoutHttps.substring(githubUrlWithoutHttps.indexOf("/") + 1);
+  const apiDownloadUrl = `https://api.${githubDomain}/repos/${ownerAndRepo}/zipball`;
+  function handleResponse(response) {
+    if (response.statusCode === 302) {
+      return https.get(requestOptionsFromUrl(response.headers.location), handleResponse);
+    } else if(response.statusCode !== 200) {
+      console.error(`Voop: Couldn't download custom scripts from repository ${repoUrl}. HTTP status code: ${response.statusCode}`);
+      return;
+    }
+    const voopExtDir = vscode.extensions.getExtension("PhilippT.voop").extensionPath;
+    if (!fs.existsSync(`${voopExtDir}/githubScripts`)) {
+      fs.mkdirSync(`${voopExtDir}/githubScripts`);
+    }
+    response
+      .on("error", (e) => {
+        console.error("Reponse read error during download of Voop custom scripts from github.", e.message);
+      })
+      .on("end", () => {
+        //1
+        // if (response.destroyed) {
+        //   console.error("Download of Voop custom scripts from github failed.");
+        // } else {
+        //   console.log("Download of Voop custom scripts from github finished.");
+        // }
+      })
+      .on("close", () => {
+        //2
+        // console.log("READER CLOSE");
+      });
+
+    const writer = fs
+      .createWriteStream(`${voopExtDir}/githubScripts/customScriptsFromGitHub.zip`)
+      .on("finish", () => {
+        //3
+        // console.log("WRITER FINISH");
+      })
+      .on("error", (e) => {
+        console.error("Write error during download of Voop custom scripts from github.", e.message);
+      })
+      .on("end", () => {
+        // console.log("WRITER END");
+      })
+      .on("close", () => {
+        //4
+        // console.log("WRITER CLOSE");
+        //delete old scripts after download of new ones
+        fs.readdir(`${voopExtDir}/githubScripts`, (err, files) => {
+          if (err) {
+            console.log(`An error occured while reading directory ${voopExtDir}/githubScripts: ${err}`);
+            return;
+          }
+          for (const file of files) {
+            if (file != "customScriptsFromGitHub.zip") {
+              fs.unlink(path.join(`${voopExtDir}/githubScripts`, file), (err) => {
+                if (err) {
+                  console.log(`An error occured while deleting file ${voopExtDir}/githubScripts/${file}: ${err}`);
+                }
+              });
+            }
+          }
+          yauzl.open(`${voopExtDir}/githubScripts/customScriptsFromGitHub.zip`, { lazyEntries: true }, function (err, zipfile) {
+            if (err) {
+              console.error(`An error occured while opening zip file ${voopExtDir}/githubScripts/customScriptsFromGitHub.zip: ${err}`);
+              return;
+            }
+            zipfile.readEntry();
+            zipfile.on("entry", function (entry) {
+              if (/\/$/.test(entry.fileName)) {
+                // directory, go on
+                zipfile.readEntry();
+              } else {
+                // file entry
+                if (entry.fileName.endsWith(".js")) {
+                  zipfile.openReadStream(entry, function (err, readStream) {
+                    if (err) throw err;
+                    readStream.on("end", function () {
+                      zipfile.readEntry();
+                    });
+                    var writeStream = fs.createWriteStream(`${voopExtDir}/githubScripts/${entry.fileName.substring(entry.fileName.lastIndexOf("/") + 1)}`);
+                    readStream.pipe(writeStream);
+                  });
+                } else {
+                  zipfile.readEntry();
+                }
+              }
+            });
+            zipfile.on("close", function () {
+              addScriptsInPath(`${voopExtDir}/githubScripts`);
+              console.log("Loaded Voop custom scripts from github repository.");
+            });
+          });
+        });
+      });
+
+    response.pipe(writer);
+  }
+  https.get(requestOptionsFromUrl(apiDownloadUrl, githubPersonalAccessToken), handleResponse);
 }
 
 function loadScripts() {
@@ -41,6 +166,10 @@ function loadScripts() {
   const settings = vscode.workspace.getConfiguration("voop");
   if (settings.customScriptsFolderLocation && settings.customScriptsFolderLocation.trim().length > 0) {
     addScriptsInPath(settings.customScriptsFolderLocation);
+  }
+  if (settings.githubCustomScriptRepository && settings.githubCustomScriptRepository.trim().length > 0) {
+    //addScriptsInPath(settings.customScriptsFolderLocation);
+    downloadRepositoryAsZip(settings.githubCustomScriptRepository, settings.githubPersonalAccessToken);
   }
 }
 
@@ -73,11 +202,11 @@ function activate(context) {
       let textToEdit = !selectedText || selectedText.length === 0 ? wholeDocumentText : selectedText;
 
       //additional input needed?
-      let userInput = '';
-      if(selectedScript.userInput){
-        userInput = await vscode.window.showInputBox({placeHolder: selectedScript.userInputPlaceHolder ? selectedScript.userInputPlaceHolder : ''});
+      let userInput = "";
+      if (selectedScript.userInput) {
+        userInput = await vscode.window.showInputBox({ placeHolder: selectedScript.userInputPlaceHolder ? selectedScript.userInputPlaceHolder : "" });
       }
-      if(userInput === undefined){
+      if (userInput === undefined) {
         //userInput was canceled, stop executing script
         return;
       }
@@ -108,7 +237,10 @@ function activate(context) {
         } else if (wholeDocumentText !== inputObj.fullText || !selectedText || selectedText.length === 0) {
           //fullText modified or no selection, replace whole document
           const textForReplacement = inputObj.text !== textToEdit ? inputObj.text : inputObj.fullText;
-          editBuilder.replace(new vscode.Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end), textForReplacement);
+          editBuilder.replace(
+            new vscode.Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end),
+            textForReplacement
+          );
         } else {
           //text is selected, replace selection
           const textForReplacement = inputObj.text !== textToEdit ? inputObj.text : inputObj.selection;
