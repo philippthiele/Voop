d11e9d317358daea2895825cc7e4248ef92724e7
Philipp Thiele <philipp.thiele@sap.com>
2022-06-02 06:58:18 +0200

    load existing downloaded scripts even on network failure

--- GitHubDownloadUtil.js
+++ GitHubDownloadUtil.js
@@ -112,10 +112,12 @@ module.exports = class GitHubDownloadUtil {
                 const req = https.get(this.requestOptionsFromUrl(apiCommitUrl, await _authSettingsInstance.getAuthData(githubDomain)), handleResponse);
                 req.on('error', error => {
                     console.error(`An error occured during request to GitHub for adding custom voop scripts: ${error}`);
+                    rejectCheck();
                 });
                 req.end();
             } else {
                 console.error(`An error occured during request to GitHub for adding custom voop scripts: ${error}`);
+                rejectCheck();
             }
         });
         req.end();
@@ -137,157 +139,159 @@ module.exports = class GitHubDownloadUtil {
         const apiDownloadUrl = `https://api.${githubDomain}/repos/${ownerAndRepo}/zipball`;
         const voopExtDir = vscode.extensions.getExtension("PhilippT.voop").extensionPath;
 
-        let latestShaOrFalse;
-        try {
-            latestShaOrFalse = await this.checkDownloadNecessary(repoUrl, githubDomain, ownerAndRepo, owner, repo)    
-        } catch (error) {
-            console.error("Couldn't check whether script download is necessary, authentication missing?");
-            if(fs.existsSync(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`)){
-                console.info("Target folder already existed, since download of newest scripts failed, loading existing one.")
-                resolveDownload(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`);
-            } else {
-                rejectDownload();
-            }            
-            return;
-        }        
-        if (latestShaOrFalse == false) {
-            resolveDownload(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`);
-            console.log(`Already downloaded Voop custom scripts from github repository: ${repoUrl}.`);
-            return downloadPromise;
-        };
-
-        const handleResponse = async function (response) {
-            if (response.statusCode === 302) {
-                const req = https.get(this.requestOptionsFromUrl(response.headers.location), handleResponse);
-                req.on('error', async (error) => {
-                    console.error(`An error occured during request to GitHub for adding custom voop scripts: ${error}`);
+        setTimeout(async function () {
+            let latestShaOrFalse;
+            try {
+                latestShaOrFalse = await this.checkDownloadNecessary(repoUrl, githubDomain, ownerAndRepo, owner, repo)
+            } catch (error) {
+                console.error("Couldn't check whether script download is necessary, authentication missing?");
+                if (fs.existsSync(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`)) {
+                    console.info("Target folder already existed, since download of newest scripts failed, loading existing one.")
+                    resolveDownload(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`);
+                } else {
                     rejectDownload();
-                });
-                req.end();
-                return;
-            } else if (response.statusCode !== 200) {
-                console.error(`Voop: Couldn't download newest custom scripts from repository ${repoUrl}. Loading old ones. HTTP status code: ${response.statusCode}`);
-                resolveDownload(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`);
+                }
                 return;
             }
-            response
-                .on("error", (e) => {
-                    console.error("Response read error during download of Voop custom scripts from github.", e.message);
-                    rejectDownload();
-                })
-                .on("end", () => {
-                    //1
-                    // if (response.destroyed) {
-                    //   console.error("Download of Voop custom scripts from github failed.");
-                    // } else {
-                    //   console.log("Download of Voop custom scripts from github finished.");
-                    // }
-                })
-                .on("close", () => {
-                    //2
-                    // console.log("READER CLOSE");
-                });
+            if (latestShaOrFalse == false) {
+                resolveDownload(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`);
+                console.log(`Already downloaded Voop custom scripts from github repository: ${repoUrl}.`);
+                return downloadPromise;
+            };
 
-            const writer = fs
-                .createWriteStream(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/customScriptsFromGitHub.zip`)
-                .on("finish", () => {
-                    //3
-                    // console.log("WRITER FINISH");
-                })
-                .on("error", (e) => {
-                    console.error("Write error during download of Voop custom scripts from github.", e.message);
-                    rejectDownload();
-                })
-                .on("end", () => {
-                    // console.log("WRITER END");
-                })
-                .on("close", () => {
-                    //4
-                    // console.log("WRITER CLOSE");
-                    //delete old scripts after download of new ones
-                    fs.readdir(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`, (err, files) => {
-                        if (err) {
-                            console.log(`An error occured while reading directory ${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}: ${err}`);
-                            return;
-                        }
-                        for (const file of files) {
-                            if (file != "customScriptsFromGitHub.zip") {
-                                fs.unlink(path.join(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`, file), (err) => {
-                                    if (err) {
-                                        console.log(`An error occured while deleting file ${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/${file}: ${err}`);
-                                    }
-                                });
-                            }
-                        }
-                        const writePromises = [];
-                        yauzl.open(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/customScriptsFromGitHub.zip`, { lazyEntries: true }, function (err, zipfile) {
+            const handleResponse = async function (response) {
+                if (response.statusCode === 302) {
+                    const req = https.get(this.requestOptionsFromUrl(response.headers.location), handleResponse);
+                    req.on('error', async (error) => {
+                        console.error(`An error occured during request to GitHub for adding custom voop scripts: ${error}`);
+                        rejectDownload();
+                    });
+                    req.end();
+                    return;
+                } else if (response.statusCode !== 200) {
+                    console.error(`Voop: Couldn't download newest custom scripts from repository ${repoUrl}. Loading old ones. HTTP status code: ${response.statusCode}`);
+                    resolveDownload(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`);
+                    return;
+                }
+                response
+                    .on("error", (e) => {
+                        console.error("Response read error during download of Voop custom scripts from github.", e.message);
+                        rejectDownload();
+                    })
+                    .on("end", () => {
+                        //1
+                        // if (response.destroyed) {
+                        //   console.error("Download of Voop custom scripts from github failed.");
+                        // } else {
+                        //   console.log("Download of Voop custom scripts from github finished.");
+                        // }
+                    })
+                    .on("close", () => {
+                        //2
+                        // console.log("READER CLOSE");
+                    });
+
+                const writer = fs
+                    .createWriteStream(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/customScriptsFromGitHub.zip`)
+                    .on("finish", () => {
+                        //3
+                        // console.log("WRITER FINISH");
+                    })
+                    .on("error", (e) => {
+                        console.error("Write error during download of Voop custom scripts from github.", e.message);
+                        rejectDownload();
+                    })
+                    .on("end", () => {
+                        // console.log("WRITER END");
+                    })
+                    .on("close", () => {
+                        //4
+                        // console.log("WRITER CLOSE");
+                        //delete old scripts after download of new ones
+                        fs.readdir(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`, (err, files) => {
                             if (err) {
-                                console.error(`An error occured while opening zip file ${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/customScriptsFromGitHub.zip: ${err}`);
-                                rejectDownload();
+                                console.log(`An error occured while reading directory ${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}: ${err}`);
                                 return;
                             }
-                            zipfile.readEntry();
-                            zipfile.on("entry", function (entry) {
-                                if (/\/$/.test(entry.fileName)) {
-                                    // directory, go on
-                                    zipfile.readEntry();
-                                } else {
-                                    // file entry
-                                    if (entry.fileName.endsWith(".js")) {
-                                        zipfile.openReadStream(entry, function (err, readStream) {
-                                            if (err) throw err;
-                                            readStream.on("end", function () {
-                                                zipfile.readEntry();
-                                            });
-                                            var writeStream = fs.createWriteStream(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/${entry.fileName.substring(entry.fileName.lastIndexOf("/") + 1)}`);
-                                            readStream.pipe(writeStream);
-                                            var promiseResolve, promiseReject;
-                                            var writePromise = new Promise(function (resolve) {
-                                                promiseResolve = resolve;
-                                            });
-                                            writePromises.push(writePromise);
-                                            writeStream.on('finish', () => {
-                                                promiseResolve();
-                                            });
-                                        });
-                                    } else {
-                                        zipfile.readEntry();
-                                    }
-                                }
-                            });
-                            zipfile.on("close", function () {
-                                Promise.all(writePromises).then(() => {
-                                    fs.writeFileSync(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/latestCommit`, latestShaOrFalse);
-                                    fs.unlink(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/customScriptsFromGitHub.zip`, (err) => {
+                            for (const file of files) {
+                                if (file != "customScriptsFromGitHub.zip") {
+                                    fs.unlink(path.join(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`, file), (err) => {
                                         if (err) {
                                             console.log(`An error occured while deleting file ${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/${file}: ${err}`);
                                         }
                                     });
-                                    resolveDownload(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`);
-                                    console.log(`Downloaded Voop custom scripts from github repository: ${repoUrl}.`);
+                                }
+                            }
+                            const writePromises = [];
+                            yauzl.open(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/customScriptsFromGitHub.zip`, { lazyEntries: true }, function (err, zipfile) {
+                                if (err) {
+                                    console.error(`An error occured while opening zip file ${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/customScriptsFromGitHub.zip: ${err}`);
+                                    rejectDownload();
+                                    return;
+                                }
+                                zipfile.readEntry();
+                                zipfile.on("entry", function (entry) {
+                                    if (/\/$/.test(entry.fileName)) {
+                                        // directory, go on
+                                        zipfile.readEntry();
+                                    } else {
+                                        // file entry
+                                        if (entry.fileName.endsWith(".js")) {
+                                            zipfile.openReadStream(entry, function (err, readStream) {
+                                                if (err) throw err;
+                                                readStream.on("end", function () {
+                                                    zipfile.readEntry();
+                                                });
+                                                var writeStream = fs.createWriteStream(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/${entry.fileName.substring(entry.fileName.lastIndexOf("/") + 1)}`);
+                                                readStream.pipe(writeStream);
+                                                var promiseResolve, promiseReject;
+                                                var writePromise = new Promise(function (resolve) {
+                                                    promiseResolve = resolve;
+                                                });
+                                                writePromises.push(writePromise);
+                                                writeStream.on('finish', () => {
+                                                    promiseResolve();
+                                                });
+                                            });
+                                        } else {
+                                            zipfile.readEntry();
+                                        }
+                                    }
+                                });
+                                zipfile.on("close", function () {
+                                    Promise.all(writePromises).then(() => {
+                                        fs.writeFileSync(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/latestCommit`, latestShaOrFalse);
+                                        fs.unlink(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/customScriptsFromGitHub.zip`, (err) => {
+                                            if (err) {
+                                                console.log(`An error occured while deleting file ${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}/${file}: ${err}`);
+                                            }
+                                        });
+                                        resolveDownload(`${voopExtDir}/githubCustomScripts/${githubDomain}-${owner}-${repo}`);
+                                        console.log(`Downloaded Voop custom scripts from github repository: ${repoUrl}.`);
+                                    });
                                 });
                             });
                         });
                     });
-                });
 
-            response.pipe(writer);
-        }.bind(this);
-        const req = https.get(this.requestOptionsFromUrl(apiDownloadUrl, await _authSettingsInstance.getAuthData(githubDomain)), handleResponse);
-        req.on('error', async (error) => {
-            if (error.code && error.code === 'ENOTFOUND') {
-                //try /api/v3 path for enterprise github
-                const apiDownloadUrl = `https://${githubDomain}/api/v3/repos/${ownerAndRepo}/zipball`;
-                const req = https.get(this.requestOptionsFromUrl(apiDownloadUrl, await _authSettingsInstance.getAuthData(githubDomain)), handleResponse);
-                req.on('error', error => {
+                response.pipe(writer);
+            }.bind(this);
+            const req = https.get(this.requestOptionsFromUrl(apiDownloadUrl, await _authSettingsInstance.getAuthData(githubDomain)), handleResponse);
+            req.on('error', async (error) => {
+                if (error.code && error.code === 'ENOTFOUND') {
+                    //try /api/v3 path for enterprise github
+                    const apiDownloadUrl = `https://${githubDomain}/api/v3/repos/${ownerAndRepo}/zipball`;
+                    const req = https.get(this.requestOptionsFromUrl(apiDownloadUrl, await _authSettingsInstance.getAuthData(githubDomain)), handleResponse);
+                    req.on('error', error => {
+                        console.error(`An error occured during request to GitHub for adding custom voop scripts: ${error}`);
+                    });
+                    req.end();
+                } else {
                     console.error(`An error occured during request to GitHub for adding custom voop scripts: ${error}`);
-                });
-                req.end();
-            } else {
-                console.error(`An error occured during request to GitHub for adding custom voop scripts: ${error}`);
-            }
-        });
-        req.end();
+                }
+            });
+            req.end();
+        }.bind(this), 0);
 
         return downloadPromise;
     }
